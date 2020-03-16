491d44bb3c50e3b7b503e35a79389e5af4ad1f1e
AmBadAl <62250863+AmBadAl@users.noreply.github.com>
2020-03-16 20:03:26 +0330

    do a change

--- src/gitWorkshop.java
+++ src/gitWorkshop.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class gitWorkshop {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("The changes have been done!");
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        System.out.println(add(num1, num2));
+    }
+    static int add(int a, int b){
+        return a + b;
     }
 }
